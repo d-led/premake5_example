257a92daef951ca7c0b17eb3d405fc5dac138093
Dmitry Ledentsov <dmlled@yahoo.com>
2016-09-18 21:33:25 +0200

    build refactoring

--- premake5.lua
+++ premake5.lua
@@ -1,17 +1,11 @@
 c = dofile 'commons.lua'
 
+-------------
 c.my_solution 'premake5_example'
 
-c.cpp_dll 'my_lib'
-    files {
-        'src/cpp/*.cpp'
-    }
+---------
+c.cpp_dll( 'my_lib', 'src/cpp' )
 
-c.cs_executable 'test'
-    files {
-        'src/cs/*.cs'
-    }
-
-    links {
-        'my_lib'
-    }
+---------------
+c.cs_executable( 'test', 'src/cs' )
+    links 'my_lib'
